77dfe87a976036db737ee2623ae62f5f7028e1e1
schuberte <schuberte@msoe.edu>
2023-12-20 20:33:50 -0600

    Implemented project.

--- src/main/test/QuizTests.java
+++ src/main/test/QuizTests.java
@@ -1,6 +1,6 @@
 /**
  * Tests consisted of running various text files of trivia questions through the program. These will
- * be committed to the repo for viewing. They featured incorrect and correct format types. I also utilized
+ * be committed to the repo for viewing (directory TestFiles). They featured incorrect and correct format types. I also utilized
  * the built-in debugger in IntelliJ Idea. Given more time I could utilize my knowledge of JUnit 5 to
  * develop additional test cases.
  */
