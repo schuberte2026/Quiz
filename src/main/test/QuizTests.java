eeb4ba28e3ac16ae733a0cb444fd2aebb848c7f9
schuberte <schuberte@msoe.edu>
2023-12-21 16:41:01 -0600

    Added tests.

--- src/main/test/QuizTests.java
+++ src/main/test/QuizTests.java
@@ -1,6 +1,22 @@
 /**
  * Tests consisted of running various text files of trivia questions through the program. These will
  * be committed to the repo for viewing (directory TestFiles). They featured incorrect and correct format types. I also utilized
- * the built-in debugger in IntelliJ Idea. Given more time I could utilize my knowledge of JUnit 5 to
- * develop additional test cases.
+ * the built-in debugger in IntelliJ Idea. Had time to create simple test in JUnit 5.
  */
+import com.example.quizkata.QuizController;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+class QuizControllerTest {
+
+    @Test
+    void testFileLoading() {
+        QuizController quizController = new QuizController();
+        quizController.setStopBeforeLoadNextQuestion(true);
+
+        String testFilePath = "C:\\Users\\schuberte\\OneDrive - Milwauk" +
+                "ee School of Engineering\\DSA\\Week 1\\QuizKata\\TestFiles\\quizQnA.txt";
+
+        assertDoesNotThrow(() -> quizController.loadFile(testFilePath));
+    }
+}
